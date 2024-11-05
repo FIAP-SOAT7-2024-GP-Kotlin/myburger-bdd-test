2543a065fc23e027c0844dd8b2a898131c119c67
Rafael Correia <rafaeljpc@gmail.com>
2024-11-05 18:56:45 -0300

    add bdd

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,13 +28,16 @@ repositories {
 
 dependencies {
     testImplementation("io.cucumber:cucumber-java:7.20.1")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine:7.20.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.platform:junit-platform-console")
+    testImplementation("org.junit.platform:junit-platform-suite")
 }
 
 tasks.test {
     useJUnitPlatform()
+    systemProperty("cucumber.junit-platform.naming-strategy", "long")
 }
 
 tasks.withType<KotlinCompile> {
@@ -43,17 +46,6 @@ tasks.withType<KotlinCompile> {
         this.freeCompilerArgs.add("-Xjsr305=strict")
         this.jvmTarget.set(kotlinJVM)
     }
-}
-
-tasks.register<JavaExec>("cucumber") {
-    group = "cucumber"
-    dependsOn(tasks.testClasses)
-    classpath(sourceSets.test.get().runtimeClasspath)
-
-    this.mainClass.set("io.cucumber.core.cli.Main")
-    this.args("--plugin", "pretty")
-    println("path = ${sourceSets.test.get().resources.srcDirs.first()}")
-    this.args("--glue", "io.github.soat7.myburgercontrol.testbdd", sourceSets.test.get().resources.srcDirs.first())
 }
 
 ktlint {
---
+++ src/test/kotlin/io/github/soat7/myburgercontrol/testbdd/CucumberRunnerTest.kt
@@ -0,0 +1,29 @@
+package io.github.soat7.myburgercontrol.testbdd
+
+import io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME
+import io.cucumber.junit.platform.engine.Constants.JUNIT_PLATFORM_NAMING_STRATEGY_PROPERTY_NAME
+import io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME
+import org.junit.platform.suite.api.ConfigurationParameter
+import org.junit.platform.suite.api.ConfigurationParameters
+import org.junit.platform.suite.api.IncludeEngines
+import org.junit.platform.suite.api.SelectClasspathResource
+import org.junit.platform.suite.api.SelectClasspathResources
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("io.github.soat7.myburgercontrol.testbdd")
+@SelectClasspathResources(
+    value = [
+        SelectClasspathResource("/"),
+    ],
+)
+@ConfigurationParameters(
+    value = [
+        ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty"),
+        ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.soat7.myburgercontrol.testbdd"),
+        ConfigurationParameter(key = JUNIT_PLATFORM_NAMING_STRATEGY_PROPERTY_NAME, value = "long"),
+    ],
+)
+class CucumberRunnerTest
